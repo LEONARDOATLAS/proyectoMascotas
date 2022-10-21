d6bbecb2b5bf69f72549468a549a46b1e6dac447
LEONARDO MERCHAN AGUILAR <leonardo.merchan.pg@gmail.com>
2022-10-21 11:03:06 -0500

    Se comidifico el modelo producto con la data

--- backend/src/models/Productos.js
+++ backend/src/models/Productos.js
@@ -1,40 +1,15 @@
 const mongoose = require('mongoose');
-const {
-    Schema
-} = mongoose;
+const { Schema } = mongoose;
 
 const ProductoSchema = new Schema({
-    id: {
-        type: Number,
-        require: true,
-        unique: true
-    },
-    codigo: {
-        type: String,
-        require: true
-    },
-    nombre: {
-        type: String,
-        requiere: true
-    },
-    categoria: {
-        type: String,
-        requiere: true
-    },
-    descripcion: {
-        type: String,
-        requiere: true
-    },
-    valor: {
-        type: Number,
-        requiere: true
-    },
-    iva: {
-        type: Number
-    },
-    estado: {
-        type: Number
-    }
+    id: {type: Number, require: true, unique: true },
+    codigo: {type: String,require: true},
+    nombre: {type: String, requiere: true },
+    categoria: {type: String,requiere: true},
+    descripcion: {type: String, requiere: true},
+    valor: {type: Number,requiere: true},
+    iva: {type: Number},
+    estado: {type: Number }
 }, {
     versionKey: false
 });
