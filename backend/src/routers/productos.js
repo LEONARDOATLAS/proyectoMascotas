1b27a3b7788b13bafd4e3ae1ae8a1ce3d427030f
LEONARDO MERCHAN AGUILAR <leonardo.merchan.pg@gmail.com>
2022-10-24 09:51:11 -0500

    Inicio del frontend del proyecto

--- backend/src/routers/productos.js
+++ backend/src/routers/productos.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const productController = require("../controllers/controllerProducto")
+const express = require('express');
+const router = express.Router();
+const productController = require("../controllers/ControllerProducto");
 
 router.get("/listar", productController.productoListar);
 router.post("/save", productController.productoAgregar)
 router.get("/listar/:id", productController.listarProductoId)
 router.post("/editar/:id", productController.productoEditar)
 router.post("/eliminar/:id", productController.productoEliminar)
+
+
+// rutas de consultas 
+const con_categorias_productos = require("../controllers/consultas/controllerProductosCategoria")
+router.get("/productoscategorias", con_categorias_productos)
+router.get("/productoscategorias/:id", con_categorias_productos)
 
 
 module.exports = router
